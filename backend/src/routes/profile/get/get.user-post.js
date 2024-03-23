b058696c1f0f87c3b2e7658a7391ae7edace5ed8
khmaladze <khmaladze.g.7@gmail.com>
2024-03-23 17:29:10 -0400

    backend update get profile post

--- backend/src/routes/profile/get/get.user-post.js
+++ backend/src/routes/profile/get/get.user-post.js
@@ -3,8 +3,10 @@ const {
   customServerError,
 } = require("../../../function/server-custom-error-response");
 const { custom_server_response } = require("../../../function/server-response");
-
 const { Post } = require("../../../models/post/post-model");
+const { User } = require("../../../models/user/user-model");
+const CommentModel = require("../../../models/post/post-comment-model");
+const { PostLike } = require("../../../models/post/post-like-model");
 
 const routeMessage = {
   get_user_post: "get user post",
@@ -23,11 +25,58 @@ const businessLogic = async (req, res) => {
       .skip(postsToSkip)
       .limit(pageSize);
 
+    // Add user information to each post object
+    const postsWithUserInfo = await Promise.all(
+      userPosts.map(async (post) => {
+        // Find user corresponding to the post author
+        const user = await User.findOne({ _id: userProfileId });
+
+        // Check if the like exists for the specified post and user
+        const isLikeExists = await PostLike.exists({
+          post_id: post._id,
+          like_author_id: userProfileId,
+        }).catch((error) => {
+          console.error("Error checking like for post:", error);
+          return false; // Set like existence to false in case of error
+        });
+
+        // Fetch comments for the post
+        const comments = await CommentModel.find({
+          post_id: post._id,
+          author_id: userProfileId,
+        });
+
+        // Fetch additional information about the users who posted each comment
+        const commentsWithUserInfo = await Promise.all(
+          comments.map(async (comment) => {
+            const commenter = await User.findOne({ _id: userProfileId });
+            return {
+              ...comment.toObject(),
+              commenter_profileImage: commenter ? commenter.profileImage : "",
+              commenter_gender: commenter ? commenter.gender : "",
+            };
+          })
+        );
+
+        return {
+          ...post.toObject(), // Convert Mongoose document to plain JavaScript object
+          liked: isLikeExists ? true : false,
+          user: {
+            profileImage: user.profileImage,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            gender: user.gender,
+          },
+          comments: commentsWithUserInfo, // Include comments in the post object
+        };
+      })
+    );
+
     return custom_server_response(
       res,
       200,
       routeMessage.get_user_post,
-      userPosts
+      postsWithUserInfo
     );
   } catch (error) {
     return customServerError(res, error);
