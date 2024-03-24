abbf31c39293d936935a1af80bfa75f106872c9d
khmaladze <khmaladze.g.7@gmail.com>
2024-03-24 01:37:00 -0400

    backend update

--- backend/src/function/server-user-profile.js
+++ backend/src/function/server-user-profile.js
@@ -1,45 +1,30 @@
-// Define a union type for Zodiac Signs
-const zodiacs = [
-  "Aquarius",
-  "Pisces",
-  "Aries",
-  "Taurus",
-  "Gemini",
-  "Cancer",
-  "Leo",
-  "Virgo",
-  "Libra",
-  "Scorpio",
-  "Sagittarius",
-  "Capricorn",
-];
-
-// Function to determine Zodiac Sign based on birth month and day
-const getZodiacSign = (month, day) => {
-  // Array of Zodiac Sign start dates in the format [month, day]
-  const zodiacDates = [
-    [1, 20],
-    [2, 19],
-    [3, 21],
-    [4, 20],
-    [5, 21],
-    [6, 21],
-    [7, 23],
-    [8, 23],
-    [9, 23],
-    [10, 23],
-    [11, 22],
-    [12, 22],
-  ];
-
-  // Find the index of the Zodiac Sign based on the birth month and day
-  const index = zodiacDates.findIndex(
-    ([zodiacMonth, zodiacDay]) => month === zodiacMonth && day >= zodiacDay
-  );
-
-  // Return the corresponding Zodiac Sign or default to Capricorn
-  return zodiacs[index !== -1 ? index : 11];
-};
+function getZodiacSign(month, day) {
+  if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+    return "Aquarius";
+  } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+    return "Pisces";
+  } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+    return "Aries";
+  } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+    return "Taurus";
+  } else if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) {
+    return "Gemini";
+  } else if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) {
+    return "Cancer";
+  } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+    return "Leo";
+  } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+    return "Virgo";
+  } else if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) {
+    return "Libra";
+  } else if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) {
+    return "Scorpio";
+  } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+    return "Sagittarius";
+  } else {
+    return "Capricorn";
+  }
+}
 
 // Function to check if a date is valid
 const isValidDate = (year, month, day) => {
